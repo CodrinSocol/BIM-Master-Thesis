caf2c3ae74d09d1f67c8becdc3d050826c26ab3a
Codrin Socol <82222825+CodrinSocol@users.noreply.github.com>
2025-06-01 19:13:01 +0200

    code and datasets with mean/std_dev for each price level for each day

---
+++ src/others/generate_daily_stats.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+
+import polars as pl
+
+from src.others.generate_rf_features import create_polars_schema
+
+def create_output_schema():
+    schema = {}
+
+    for i in range(0,25):
+        schema[f'asks[{i}].price_mean'] = pl.Float64
+        schema[f'asks[{i}].price_std_dev'] = pl.Float64
+        schema[f'asks[{i}].amount_mean'] = pl.Float64
+        schema[f'asks[{i}].amount_std_dev'] = pl.Float64
+        schema[f'bids[{i}].price_mean'] = pl.Float64
+        schema[f'bids[{i}].price_std_dev'] = pl.Float64
+        schema[f'bids[{i}].amount_mean'] = pl.Float64
+        schema[f'bids[{i}].amount_std_dev'] = pl.Float64
+
+    return pl.Schema(schema)
+
+def generate_daily_stats():
+    book_snapshot_dir = "../../data/daily_tardis/book_snapshot_25"
+
+    df_schema = create_polars_schema()
+    out_schema = create_output_schema()
+
+    columns = []
+
+    for i in range(0, 25):
+        columns.append(f'asks[{i}].price')
+        columns.append(f'asks[{i}].amount')
+        columns.append(f'bids[{i}].price')
+        columns.append(f'bids[{i}].amount')
+
+
+    for i in range(20,32):
+        print(f"Processing day {i:02d}...")
+        df = pl.read_csv(Path(f"{book_snapshot_dir}/deribit_book_snapshot_25_2025-01-{i:02d}_ETH-PERPETUAL.csv.gz"), columns=columns, schema=df_schema, ignore_errors=True, encoding='utf8-lossy')
+        new_row = {}
+        for level in range(0,25):
+
+            ask_price_mean = df[f'asks[{level}].price'].mean()
+            ask_price_std_dev = df[f'asks[{level}].price'].std()
+            ask_amount_mean = df[f'asks[{level}].amount'].mean()
+            ask_amount_std_dev = df[f'asks[{level}].amount'].std()
+
+            bid_price_mean = df[f'bids[{level}].price'].mean()
+            bid_price_std_dev = df[f'bids[{level}].price'].std()
+            bid_amount_mean = df[f'bids[{level}].amount'].mean()
+            bid_amount_std_dev = df[f'bids[{level}].amount'].std()
+
+            new_row[f'asks[{level}].price_mean'] = ask_price_mean
+            new_row[f'asks[{level}].price_std_dev'] = ask_price_std_dev
+            new_row[f'asks[{level}].amount_mean'] = ask_amount_mean
+            new_row[f'asks[{level}].amount_std_dev'] = ask_amount_std_dev
+
+            new_row[f'bids[{level}].price_mean'] = bid_price_mean
+            new_row[f'bids[{level}].price_std_dev'] = bid_price_std_dev
+            new_row[f'bids[{level}].amount_mean'] = bid_amount_mean
+            new_row[f'bids[{level}].amount_std_dev'] = bid_amount_std_dev
+
+        out_df = pl.DataFrame(new_row, schema=out_schema)
+        out_df.write_csv(f"../../data-generated/depth-stats/daily_stats_25_{i}.csv")
+
+def merge_daily_stats():
+    schema = create_output_schema()
+
+    out_df = pl.DataFrame(schema=schema)
+    for i in range(1, 32):
+        daily_df = pl.read_csv(f"../../data-generated/depth-stats/daily_stats_25_{i}.csv", schema=schema)
+        out_df = pl.concat([out_df, daily_df], how='vertical')
+    out_df.write_csv("../../data-generated/depth-stats/daily_stats_25_merged.csv")
+
+if __name__ == "__main__":
+    # generate_daily_stats()
+    merge_daily_stats()
